153d0d28ebc0d87b21b4474c5216547c97870b14
Alpay Kılıç <alpaykilic@hotmail.com>
2019-05-29 23:12:17 +0300

    Add project files.

---
+++ TrendXamarin/TrendXamarin/MainPage.xaml.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TrendXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+           
+        }
+
+        private void GeriButon_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void GeriButon2_Clicked(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void ErkekButon_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void KadınButon_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void GeriButon3_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Ürün1Buton_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Ürün2Buton_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Ürün3Buton_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Ürün4Buton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Ürün5Buton_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Ürün6Buton_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Ürün7Buton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Ürün8Buton_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void SipButon1_Clicked(object sender, EventArgs e)
+        {         
+
+        }
+
+        private void GeriButon4_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SipButon2_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void GeriButon5_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void SipButon3_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void GeriButon6_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void SipButon4_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void GeriButon7_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void SipButon5_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void GeriButon8_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void SipButon6_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void GeriButon9_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void SipButon7_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void GeriButon10_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SipButon8_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ÖdButon_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void KadınKButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ErkekKButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ÇocukKButton_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void YaşamKButton_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void HızlıKButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void KadınKotButon_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void ErkekAvvaBut_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void ASayfaBut_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void KatBut_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void FavBut_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void SepetBut_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void HesapBut_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void SatinBasla_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void İndText_Focused(object sender, FocusEventArgs e)
+        {
+           
+        } 
+
+        private void AvvaKat_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void KotonKat_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void KategoriKadın_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void KategoriErkek_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void OnaylaBut_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void FavButon_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
